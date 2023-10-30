3a132926f531931fbc3c21fcb152d5fe4d87be49
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2023-10-30 00:04:54 -0600

    adds mongoose-autopopulate

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,7 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/open-ds'),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/open-ds', {
+      connectionFactory: (connection) => {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        connection.plugin(require('mongoose-autopopulate'));
+        return connection;
+      },
+    }),
     ProductsModule,
   ],
 })
