7641c33a50209aae30fc3c30d9ffaf33708cd66b
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2023-10-30 00:04:21 -0600

    register schemas

--- api/src/products/products.module.ts
+++ api/src/products/products.module.ts
@@ -3,10 +3,22 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Product, ProductSchema } from './schemas/product.schema';
+import {
+  ProductPrice,
+  ProductPriceSchema,
+} from './schemas/product-price.schema';
+import {
+  ProductShipping,
+  ProductShippingSchema,
+} from './schemas/product-shipping.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    MongooseModule.forFeature([
+      { name: Product.name, schema: ProductSchema },
+      { name: ProductPrice.name, schema: ProductPriceSchema },
+      { name: ProductShipping.name, schema: ProductShippingSchema },
+    ]),
   ],
   controllers: [ProductsController],
   providers: [ProductsService],
